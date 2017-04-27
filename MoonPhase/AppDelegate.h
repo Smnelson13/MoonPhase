d0e1c07e5ff99d880179c0a676d0a4b4841a2de3
Shane Nelson <shanenelson@shanes-macbook-pro.local>
2017-04-27 09:03:39 -0400

    Initial Commit

---
+++ MoonPhase/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MoonPhase
+//
+//  Created by Shane Nelson on 4/27/17.
+//  Copyright © 2017 Shane Nelson. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ MoonPhase/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MoonPhase
+//
+//  Created by Shane Nelson on 4/27/17.
+//  Copyright © 2017 Shane Nelson. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
